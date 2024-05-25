21e8acee815bddd88ef0145377141d0229cc08d0
Bleu Serge Alain <reelsergealain@gmail.com>
2024-05-25 20:03:12 +0000

    Installation d'Alpine Js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 import './bootstrap';
+
+// Démarache d'Alpine Js
+import Alpine from 'alpinejs';
+Alpine.start()
+
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"

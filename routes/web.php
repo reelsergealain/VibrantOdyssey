a8659f66791e702bdc2235fc1a7665530305d7f2
Bleu Serge Alain <reelsergealain@gmail.com>
2024-05-29 18:50:30 +0000

    Systheme de connection, deconnection

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ $slug = '[0-9a-z\-]+';
 $id = '[0-9]+';
 
 # Post Controller
-Route::get('/',[PostController::class, 'index'])->name('posts.index');
-Route::get('/article/{slug}-{post}',[PostController::class, 'show'])->name('posts.show')->where([
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/article/{slug}-{post}', [PostController::class, 'show'])->name('posts.show')->where([
     'slug' => $slug,
     'post' => $id,
 ]);
@@ -23,16 +23,16 @@ Route::get('/categorie/{category}', PostByCategoryController::class)->name('post
 // Route __invoke pour voir les Tags
 Route::get('/etiquette/{tag}', PostByTagController::class)->name('postByTag');
 
-Route::get('/register', [RegisterController::class,'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class,'register']);
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
-Route::get('/login', [RegisterController::class,'showLoginForm'])->name('login');
-Route::post('/login', [RegisterController::class,'login']);
+Route::get('/login', [RegisterController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [RegisterController::class, 'login']);
 
-Route::post('/logout', [RegisterController::class,'logout'])->name('logout');
+Route::post('/logout', [RegisterController::class, 'logout'])->name('logout');
 
-Route::get('/home', [HomeController::class,'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/logout', function () {
-        return to_route('posts.index');
+    return to_route('posts.index');
 });
